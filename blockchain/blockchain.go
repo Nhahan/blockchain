eac3780ad1c95d38e5f4660441b38775ee890c95
Nhahan <sun@codebrick.co>
2021-12-25 21:48:33 +0900

    check blocks

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -7,9 +7,9 @@ import (
 )
 
 type block struct {
-	data     string
-	hash     string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockchain struct {
@@ -21,12 +21,16 @@ var once sync.Once
 
 // Method
 func (b *block) calculateHash() {
-	hash := sha256.Sum256([]byte(b.data + b.prevHash))
-	b.hash = fmt.Sprintf("%x", hash)
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
 }
 
 func (b *blockchain) AddBlock(data string) {
-	b.blocks = append(b.blocks, createBlock("Genesis Block"))
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
 }
 
 // Function
@@ -35,7 +39,7 @@ func getLastHash() string {
 	if totalBlocks == 0 {
 		return ""
 	}
-	return GetBlockchain().blocks[totalBlocks-1].hash
+	return GetBlockchain().blocks[totalBlocks-1].Hash
 }
 
 func createBlock(data string) *block {
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/Nhahan/blockchain/blockchain"
+import (
+	"fmt"
+
+	"github.com/Nhahan/blockchain/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
 	chain.AddBlock("Second Block")
 	chain.AddBlock("Third Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Println(block.Data)
+	}
 }
