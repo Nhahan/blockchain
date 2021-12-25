61bec718feeb90dce1ae8afe09df0f68f0898f20
Nhahan <sun@codebrick.co>
2021-12-25 20:12:09 +0900

    first blockchain

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 type block struct {
 	data     string
@@ -8,7 +11,34 @@ type block struct {
 	prevHash string
 }
 
+type blockchain struct {
+	blocks []block
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+}
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data: %s\n", block)
+	}
+}
+
 func main() {
-	genesisBlock := block{"Genesis Block", "", ""}
-	genesisBlock.hash = sha256.Sum256([]byte(genesisBlock.data + genesisBlock.hash))
+	chain := blockchain{}
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+	chain.listBlocks()
 }
