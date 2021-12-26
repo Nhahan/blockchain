b69f1abb9bb4d97b9d6c54dacbf049671ac4fef3
Nhahan <sun@codebrick.co>
2021-12-26 17:00:42 +0900

    template init

--- main.go
+++ main.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 const port string = ":4000"
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello!")
+	newTemplate, err := template.ParseFiles("templates/home.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	newTemplate.Execute()
 }
 
 func main() {
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
