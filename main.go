afb6aeff4f5837cc62114283c7e2e5ca64d7e37d
Nhahan <sun@codebrick.co>
2021-12-26 20:52:42 +0900

    delete newTemplate

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 	"github.com/Nhahan/blockchain/blockchain"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
@@ -18,12 +23,13 @@ type homeData struct {
 
 func home(rw http.ResponseWriter, r *http.Request) {
 	// Must is the function that if there's an error returns error, otherwise returns value
-	newTemplate := template.Must(template.ParseFiles("templates/home.html"))
 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
 	newTemplate.Execute(rw, data)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.html"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.html"))
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	err := http.ListenAndServe(port, nil)
