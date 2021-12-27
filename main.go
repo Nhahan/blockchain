15a430cfc84519c0c7812ae083ee1dcdd0e16442
Nhahan <sun@codebrick.co>
2021-12-27 23:06:43 +0900

    add block btn

--- main.go
+++ main.go
@@ -26,10 +26,15 @@ func home(rw http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(rw, "home", data)
 }
 
+func add( http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(rw, "add", nil)
+}
+
 func main() {
 	templates = template.Must(template.ParseGlob(templateDir + "pages/*.html"))
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.html"))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
