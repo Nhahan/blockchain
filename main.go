a24b76dfb4c8462caf8c2b7d084ab19b0e9e7169
Nhahan <sun@codebrick.co>
2021-12-29 23:09:46 +0900

    URLDescription struct

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 
 const port string = ":4000"
 
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 
 }
