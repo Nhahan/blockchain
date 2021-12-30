38eef24d7d7daff8ab2a5f54688f9cbc24dede5f
Nhahan <sun@codebrick.co>
2021-12-30 18:48:31 +0900

    MarshalText()

--- main.go
+++ main.go
@@ -9,26 +9,29 @@ import (
 
 const port string = ":4000"
 
-type URLDescription struct {
-	URL         string `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"description"` // if add ~,omitempty"` this data could be not shown
-	Payload     string `json:"payload,omitempty"`
+type URL string
+
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
 }
 
-func (u URLDescription) String() string {
-	return "Hello I'm the URL Description"
+type URLDescription struct {
+	URL         URL    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL:         "/",
+			URL:         URL("/"),
 			Method:      "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL:         "/blocks",
+			URL:         URL("/blocks"),
 			Method:      "POST",
 			Description: "Add A Block",
 			Payload:     "data:string",
@@ -40,6 +43,6 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", documentation)
-	fmt.Printf("Listening on http://localhost%s\n", port)
+	fmt.Printf("Listening on http://localhost%s", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
