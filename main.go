0ab275ecb1faeb519d819e7877cfb0aff17993b8
Nhahan <sun@codebrick.co>
2022-01-01 22:35:28 +0900

    port for explorer

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/Nhahan/blockchain/rest"
+import (
+	"github.com/Nhahan/blockchain/deprecated/explorer"
+	"github.com/Nhahan/blockchain/rest"
+)
 
 func main() {
+	explorer.Start(3000)
 	rest.Start(4000)
 }
