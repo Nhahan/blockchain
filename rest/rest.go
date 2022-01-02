a354556265dfd7e583f47a562ebd53f48775704c
Nhahan <sun@codebrick.co>
2022-01-02 19:03:03 +0900

    mux router

--- rest/rest.go
+++ rest/rest.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/Nhahan/blockchain/blockchain"
 	"github.com/Nhahan/blockchain/utils"
+	"github.com/gorilla/mux"
 )
 
 var port string
@@ -66,11 +67,16 @@ func blocks(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func block(rw http.ResponseWriter, r *http.Request) {
+
+}
+
 func Start(aPort int) {
-	handler := http.NewServeMux()
+	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
-	handler.HandleFunc("/", documentation)
-	handler.HandleFunc("/blocks", blocks)
+	router.HandleFunc("/", documentation).Methods("GET")
+	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
+	router.HandleFunc("/blocks/{id:[0-9]+", block).Methods("GET")
 	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, handler))
+	log.Fatal(http.ListenAndServe(port, router))
 }
