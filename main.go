0930e4c7976641d8ee0b29787c2331681c8527c0
Nhahan <sun@codebrick.co>
2022-01-20 23:46:16 +0900

    what's inside the flag

--- main.go
+++ main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+	fmt.Println(os.Args)
+
 	if len(os.Args) < 2 {
 		usage()
 	}
+
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+
+	portFlag := rest.Int("port", 4000, "Sets the port of the server (default 4000)")
 
 	switch os.Args[1] {
 	case "explorer":
 		fmt.Println("Start Explorer")
 	case "rest":
+		rest.Parse(os.Args[2:])
 		fmt.Println("start REST API")
 	default:
 		usage()
 	}
+
+	fmt.Println(*portFlag)
 }
 
 func usage() {
