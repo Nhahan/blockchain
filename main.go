cc427958ef5d8297ff9ca702cb30f37851e1ad38
Nhahan <sun@codebrick.co>
2022-01-22 23:16:03 +0900

    temp CLI

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+
+	explorer "github.com/Nhahan/blockchain/explorer/templates"
+	"github.com/Nhahan/blockchain/rest"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) == 1 {
 		usage()
 	}
+
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "reset", "Choose between  'html' and 'rest'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		rest.Start(*port)
+	case "html":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+
+	fmt.Println(*port, *mode)
 
 }
 
 func usage() {
-	fmt.Printf("Welcome. Please use the following commands:\n")
-	fmt.Printf("explorer:   Start the HTML Explorer\n")
-	fmt.Printf("rest: 		Start the REST API\n")
-	os.Exit(1)
+	fmt.Printf("")
+	fmt.Printf("Please use the following commands:\n")
+	fmt.Printf("-port: Set the PORT of the server\n")
+	fmt.Printf("-mode: Choose between 'html' and 'rest'\n")
+	os.Exit(0)
 }
