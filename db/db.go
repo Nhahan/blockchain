48e1cc7627b12c5e6e552ffec901c1b8e6576f03
Nhahan <sun@codebrick.co>
2022-01-23 21:48:29 +0900

    bolt db

--- db/db.go
+++ db/db.go
@@ -5,13 +5,26 @@ import (
 	bolt "go.etcd.io/bbolt"
 )
 
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
 var db *bolt.DB
 
 func DB() *bolt.DB {
 	if db == nil {
-		dbPointer, err := bolt.Open("blockcahin.db", 0600, nil)
+		db, err := bolt.Open(dbName, 0600, nil)
 		utils.HandleErr(err)
-		db = dbPointer
+
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err = t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+		utils.HandleErr(err)
 	}
 	return db
 }
