04de60b2b5d92880dd4ddcf3d5957b0bd46e5105
Nhahan <sun@codebrick.co>
2022-02-01 15:30:29 +0900

    restoring block and refatoring

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -1,8 +1,6 @@
 package blockchain
 
 import (
-	"bytes"
-	"encoding/gob"
 	"fmt"
 	"sync"
 
@@ -19,8 +17,7 @@ var b *blockchain
 var once sync.Once
 
 func (b *blockchain) restore(data []byte) {
-	decoder := gob.NewDecoder(bytes.NewReader(data))
-	utils.HandleErr(decoder.Decode(b))
+	utils.FromBytes(b, data)
 }
 
 func (b *blockchain) persist() {
