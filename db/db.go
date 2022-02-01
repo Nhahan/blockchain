a2b291bb78f53057c011b754136d15d25217f3ab
Nhahan <sun@codebrick.co>
2022-02-01 15:08:16 +0900

    db bug fix

--- db/db.go
+++ db/db.go
@@ -19,11 +19,11 @@ var db *bolt.DB
 
 func DB() *bolt.DB {
 	if db == nil {
-		db, err := bolt.Open(dbName, 0600, nil)
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		db = dbPointer
 		utils.HandleErr(err)
-
 		err = db.Update(func(t *bolt.Tx) error {
-			_, err = t.CreateBucketIfNotExists([]byte(dataBucket))
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
 			utils.HandleErr(err)
 			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
 			return err
